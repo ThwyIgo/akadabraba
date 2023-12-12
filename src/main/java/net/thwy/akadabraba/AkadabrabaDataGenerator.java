00631bacb183faa9e1aaf1e8d845eeaad57cd171
ThwyIgo <thiagopachecorocha@hotmail.com>
2023-12-12 13:37:30 -0300

    Initial commit

---
+++ src/main/java/net/thwy/akadabraba/AkadabrabaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.thwy.akadabraba;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AkadabrabaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
